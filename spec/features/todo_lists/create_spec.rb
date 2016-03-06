24d8bff5355dbf1f43c7137e7d843d5af03cf591
Leo Picado <leopic@gmail.com>
2016-03-06 11:24:10 -0600

    Small refactor.

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,71 +1,54 @@
 require 'spec_helper'
 
 describe 'Creating todo lists' do
-  it 'redirects to the homepage on success' do
-    visit 'todo_lists/'
-    click_link 'New Todo list'
-    expect(page).to have_content('New Todo List')
 
-    fill_in "Title", with: "Mi todo"
-    fill_in "Description", with: "aprender rails"
+  def create_todo_list(options={})
+    options[:title] ||= "Title"
+    options[:description] ||= "Description"
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create Todo list"
+  end
+
+  it 'redirects to the homepage on success' do
+    create_todo_list({:title => "Mi todo", :description => "aprender rails"})
 
     expect(page).to have_content("Mi todo")
   end
 
   it "displays an error when the title is skipped" do
-    visit "todo_lists/"
-    click_link 'New Todo list'
-
-    fill_in "Title", with: nil
-    fill_in "Description", with: "aprender rails"
-    click_button "Create Todo list"
+    create_todo_list({:title => ''})
 
     expect(page).to have_content("Title can't be blank")
     expect(TodoList.count).to eq(0)
-
     expect(page).to_not have_content("Mi todo")
   end
 
   it "fails when the title length is less than 3 characters" do
-    visit "todo_lists/"
-    click_link 'New Todo list'
-
-    fill_in "Title", with: "123"
-    fill_in "Description", with: "aprender rails"
-    click_button "Create Todo list"
+    create_todo_list({:title => '123', :description => 'aprender rails'})
 
     expect(page).to have_content("Title is too short")
     expect(TodoList.count).to eq(0)
-
     expect(page).to_not have_content("Mi todo")
   end
 
   it "displays an error when the description is skipped" do
-    visit "todo_lists/"
-    click_link 'New Todo list'
-
-    fill_in "Title", with: "1234"
-    fill_in "Description", with: nil
-    click_button "Create Todo list"
+    create_todo_list({:description => ''})
 
     expect(page).to have_content("Description can't be blank")
     expect(TodoList.count).to eq(0)
-
     expect(page).to_not have_content("Mi todo")
   end
 
-  it "fails when the description length is less than 3 characters" do
-    visit "todo_lists/"
-    click_link 'New Todo list'
-
-    fill_in "Title", with: "valid title"
-    fill_in "Description", with: "123"
-    click_button "Create Todo list"
+  it "fails when the description length is less than 4 characters" do
+    create_todo_list({:description => '123'})
 
     expect(page).to have_content("Description is too short")
     expect(TodoList.count).to eq(0)
-
     expect(page).to_not have_content("Mi todo")
   end
 
