9a55ba5796adb60f650efcfc1b7f7ff5c336e816
Leo Picado <leopic@gmail.com>
2016-03-06 13:03:15 -0600

    Adding specs for list_item_index

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -3,10 +3,20 @@ require 'spec_helper'
 describe "Viewing todo items" do
   let!(:todo_list) { todo_list = TodoList.create(title: 'Comida', description: 'Comida and such') }
 
-  it "displays no items when a todo list is empty" do
+  # Before each test runs
+  before do
     visit "/todo_lists"
     within "#todo_list_#{todo_list.id}" do
       click_link "Items"
     end
   end
+
+  it "displays the title of the associated list" do
+    expect(page).to have_content(todo_list.title)
+  end
+
+  it "displays no items when a todo list is empty" do
+    expect(page.all("ul.todo_items li").size).to eq 0
+  end
+
 end
