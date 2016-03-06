ac242ea43f8a367e35d3dba0e7447f400d224fc1
Leo Picado <leopic@gmail.com>
2016-03-06 12:06:48 -0600

    rspec is awesome.

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe 'Editing todo lists' do
+  # Runs in the setup phase, creates a :todo_list variable before each run
+  let!(:todo_list) { todo_list = TodoList.create(title: 'Comida', description: 'Comida and such') }
 
-  it "updates a todo list properly" do
-    todo_list = TodoList.create(title: 'Comida', description: 'Comida and such')
+  def update_todo_list(options={})
+    options[:title] ||= "Title"
+    options[:description] ||= "Description"
+    todo_list = options[:todo_list]
 
     visit "/todo_lists"
     within "#todo_list_#{todo_list.id}" do
       click_link "Edit"
     end
-    expect(page).to have_content "Editing Todo List"
 
-    fill_in "Title", with: "New title"
-    fill_in "Description", with: "New description"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Update Todo list"
+  end
+
+  it "updates a todo list properly" do
+    update_todo_list :title => "New title", :description => "New description", :todo_list => todo_list
+
     expect(page).to have_content "Todo list was successfully updated."
 
     visit "/todo_lists"
@@ -22,6 +30,26 @@ describe 'Editing todo lists' do
     todo_list.reload
     expect(todo_list.title).to eq "New title"
     expect(todo_list.description).to eq "New description"
+  end
+
+  it "fails to update with no title" do
+    update_todo_list :title => '', :todo_list => todo_list
+    expect(page).to have_content "Title can't be blank"
+  end
+
+  it "fails to update with short title" do
+    update_todo_list :title => '123', :todo_list => todo_list
+    expect(page).to have_content "Title is too short"
+  end
+
+  it "fails to update with no description" do
+    update_todo_list :description => '', :todo_list => todo_list
+    expect(page).to have_content "Description can't be blank"
+  end
+
+  it "fails to update with short title" do
+    update_todo_list :description => '123', :todo_list => todo_list
+    expect(page).to have_content "Description is too short"
   end
 
 end
