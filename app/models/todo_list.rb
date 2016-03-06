f09666174b4dbddaf36d551527835d33bd35e931
Leo Picado <leopic@gmail.com>
2016-03-06 11:09:28 -0600

    Adding new spec.

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,4 @@
 class TodoList < ActiveRecord::Base
   validates :title, presence: true
+  validates :title, length: { minimum: 4 }
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -17,11 +17,25 @@ describe 'Creating todo lists' do
     visit "todo_lists/"
     click_link 'New Todo list'
 
-    fill_in "Title", with: ""
+    fill_in "Title", with: nil
     fill_in "Description", with: "aprender rails"
     click_button "Create Todo list"
 
     expect(page).to have_content("Title can't be blank")
+    expect(TodoList.count).to eq(0)
+
+    expect(page).to_not have_content("Mi todo")
+  end
+
+  it "fails when the title length is less than 3 characters" do
+    visit "todo_lists/"
+    click_link 'New Todo list'
+
+    fill_in "Title", with: "123"
+    fill_in "Description", with: "aprender rails"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("Title is too short")
     expect(TodoList.count).to eq(0)
 
     expect(page).to_not have_content("Mi todo")
