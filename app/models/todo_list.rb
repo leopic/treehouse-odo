019993c7fbfe51b85b212a9f63d28bb0274db1e2
Leo Picado <leopic@gmail.com>
2016-03-06 11:13:12 -0600

    Adding more tests

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
   validates :title, presence: true
   validates :title, length: { minimum: 4 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 4 }
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -41,4 +41,32 @@ describe 'Creating todo lists' do
     expect(page).to_not have_content("Mi todo")
   end
 
+  it "displays an error when the description is skipped" do
+    visit "todo_lists/"
+    click_link 'New Todo list'
+
+    fill_in "Title", with: "1234"
+    fill_in "Description", with: nil
+    click_button "Create Todo list"
+
+    expect(page).to have_content("Description can't be blank")
+    expect(TodoList.count).to eq(0)
+
+    expect(page).to_not have_content("Mi todo")
+  end
+
+  it "fails when the description length is less than 3 characters" do
+    visit "todo_lists/"
+    click_link 'New Todo list'
+
+    fill_in "Title", with: "valid title"
+    fill_in "Description", with: "123"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("Description is too short")
+    expect(TodoList.count).to eq(0)
+
+    expect(page).to_not have_content("Mi todo")
+  end
+
 end
