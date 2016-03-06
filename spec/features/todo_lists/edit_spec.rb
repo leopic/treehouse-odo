19b81fa0b59f646dddcdd61e3cef3c2f4cbc5eef
Leo Picado <leopic@gmail.com>
2016-03-06 11:51:22 -0600

    Adding editing specs.

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'Editing todo lists' do
+
+  it "updates a todo list properly" do
+    todo_list = TodoList.create(title: 'Comida', description: 'Comida and such')
+
+    visit "/todo_lists"
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Edit"
+    end
+    expect(page).to have_content "Editing Todo List"
+
+    fill_in "Title", with: "New title"
+    fill_in "Description", with: "New description"
+    click_button "Update Todo list"
+    expect(page).to have_content "Todo list was successfully updated."
+
+    visit "/todo_lists"
+    expect(page).to have_content "New title"
+  end
+
+end
